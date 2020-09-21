0cc3dd74956d106ab7e9f9769a02d6c7a16e94d1
dvbasyuk <dvbasyuk@mail.rugit config --global user.name dvbasyukgit config --global user.email dvbasyuk@mail.ru>
2020-09-21 17:05:12 +0300

    Updates

---
+++ precache-manifest.36d53f80bb39ddfcb704764e5fd0e86b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bad3c8ab01f1805afcf280400e6a09e",
+    "url": "/first/index.html"
+  },
+  {
+    "revision": "549eed207ec0084bd9c7",
+    "url": "/first/static/css/3.2f708caa.chunk.css"
+  },
+  {
+    "revision": "c450026cfdc4b4a9c587",
+    "url": "/first/static/css/4.8ae3b510.chunk.css"
+  },
+  {
+    "revision": "e914a5576e5295cfdd49",
+    "url": "/first/static/css/main.42947278.chunk.css"
+  },
+  {
+    "revision": "90a2d6d98c8ae4799437",
+    "url": "/first/static/js/2.ef8bf066.chunk.js"
+  },
+  {
+    "revision": "549eed207ec0084bd9c7",
+    "url": "/first/static/js/3.dbd3467c.chunk.js"
+  },
+  {
+    "revision": "c450026cfdc4b4a9c587",
+    "url": "/first/static/js/4.c274bd0c.chunk.js"
+  },
+  {
+    "revision": "e914a5576e5295cfdd49",
+    "url": "/first/static/js/main.66cafe3c.chunk.js"
+  },
+  {
+    "revision": "571e66da398e5a23e05b",
+    "url": "/first/static/js/runtime~main.64ba3b11.js"
+  },
+  {
+    "revision": "c336831c1170b33d9153c0f54182f7cf",
+    "url": "/first/static/media/loading-gif.c336831c.gif"
+  },
+  {
+    "revision": "9fb36427bdb3325da84b7fb854856954",
+    "url": "/first/static/media/photo.9fb36427.jpg"
+  }
+]);
